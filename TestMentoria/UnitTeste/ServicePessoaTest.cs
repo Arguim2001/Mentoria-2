1bfc1880c3ac9c3731b56c45aa0c7501c9082aac
Arguim2001 <argu.aguiar@gmail.com>
2022-11-10 17:14:32 -0300

    atualizar

---
+++ TestMentoria/UnitTeste/ServicePessoaTest.cs
@@ -0,0 +1,21 @@
+﻿using Mentoria.Repository.Interface;
+using Mentoria.Service;
+using Moq;
+using Xunit;
+
+namespace MentoriaTest.UnitTests
+{
+    public class ServicePessoaTest
+    {
+        private ServicePessoa _servicePessoa;
+        private readonly Mock<IPessoaRepository> _pessoaRepository = new();
+
+        [Fact]
+
+        public void Test ()
+        {
+            string a = "argu";
+            Assert.Contains(a, "argu");
+        }
+    }
+}
